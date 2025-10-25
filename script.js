116bf467ae452bdb77a3d8d59f9441fc48903b91
sayyadshahi8252 <167566299+sayyadshahi8252@users.noreply.github.com>
2025-10-25 10:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this.species=species;
-	}
-	get species(){
-		return this.species;
-	}
-	makeSound(){
-		console.log(`The ${this.species} makes a sound`)
-	}
-}
+  constructor(species) {
+    this._species = species; // use an internal property
+  }
 
-class Dog extends Animal {
-	purr(){
-		console.log("purr")
-	}
+  get species() {
+    return this._species; // return the internal property
+  }
+
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
 class Cat extends Animal {
-	bark(){
-		console.log("woof")
-	}
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
 }
 
 // Do not change the code below this line
